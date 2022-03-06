27e60f20664929fff1d19e419ba31b94e71badfb
Aditya Mohapatra <75691553+Aditya5281@users.noreply.github.com>
2022-03-06 09:25:44 +0530

    Add files via upload

---
+++ String  and String_Buffer Assignment/src/Assignment3Q1.java
@@ -0,0 +1,11 @@
+
+public class Assignment3Q1 {
+
+	public static void main(String[] args) {
+		
+         
+	String str = new String("Hello World");
+	
+    System.out.print(str.length());
+    }
+}
---
+++ String  and String_Buffer Assignment/src/Assignment3Q3b.java
@@ -0,0 +1,9 @@
+
+public class Assignment3Q3b {
+
+	public static void main(String[] args) {
+		String sc = new String("Java String pool refers to collection of Strings which are stored in heap memory");
+        System.out.print(sc.toUpperCase());
+	}
+
+}
